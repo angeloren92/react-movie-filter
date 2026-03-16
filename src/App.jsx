5e16cdae17ade1782dcaea83f07e0463be49bb84
angeloren92 <angeloren92@gmail.com>
2026-03-16 11:33:02 +0100

    add main

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import AppMain from './components/AppMain'
+
 function App() {
+
+ const movies = [
+   { title: 'Inception', genre: 'Fantascienza' },
+   { title: 'Il Padrino', genre: 'Thriller' },
+   { title: 'Titanic', genre: 'Romantico' },
+   { title: 'Batman', genre: 'Azione' },
+   { title: 'Interstellar', genre: 'Fantascienza' },
+   { title: 'Pulp Fiction', genre: 'Thriller' },
+ ]
 
   return (
     <>
-      <h1 className="text-danger">prova</h1>
+      <AppMain movies={movies} />
     </>
   )
 }
---
+++ src/components/AppMain.jsx
@@ -0,0 +1,8 @@
+export default function AppMain({movies}) {
+
+    return(
+        <main>
+            <h1 className="text-danger">prova</h1>
+        </main>
+    )
+}
