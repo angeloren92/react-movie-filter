852d0a7c9c32cb2d304ed8d9111ef2b6376de429
angeloren92 <angeloren92@gmail.com>
2026-03-17 01:03:41 +0100

    add inputs checks control

--- src/components/ListApp/AppAddMovie.jsx
+++ src/components/ListApp/AppAddMovie.jsx
@@ -1,20 +1,45 @@
 import { useState } from "react"
 
-export default function AppAddMovies({setCopyMovies, copyMovies}) {
+export default function AppAddMovies({ setCopyMovies, copyMovies }) {
 
     const [newElement, setNewElement] = useState({ title: '', genre: '' })
+    const [errorTitle, setErrorTitle] = useState(false)
+    const [errorGenre, setErrorGenre] = useState(false)
 
     function handleAddItem(e) {
         e.preventDefault()
-        setCopyMovies([newElement, ...copyMovies])
-        setNewElement({ title: '', genre: '' })
+        if (newElement.title.length >= 5 && newElement.genre.length >= 5) {
+            setCopyMovies([newElement, ...copyMovies])
+            setNewElement({ title: '', genre: '' })
+            setErrorTitle(false)
+            setErrorGenre(false)
+        } else if (newElement.title.length >= 5 && newElement.genre.length < 5) {
+            setErrorTitle(false)
+            setErrorGenre(true)
+        } else if (newElement.genre.length >= 5 && newElement.title.length < 5) {
+            setErrorGenre(false)
+            setErrorTitle(true)
+        } else {
+            setErrorTitle(true)
+            setErrorGenre(true)
+        }
     }
 
     return (
-        <form className="d-flex" onSubmit={handleAddItem}>
-            <input type="text" placeholder="Inserisci nuovo Titolo" value={newElement.title} onChange={(e) => setNewElement({ ...newElement, title: e.target.value })} />
-            <input type="text" placeholder="Inserisci genere" value={newElement.genre} onChange={(e) => setNewElement({ ...newElement, genre: e.target.value })} />
-            <button type="submit" className="btn btn-outline-secondary">Aggiungi</button>
+        <form className="d-flex gap-4" onSubmit={handleAddItem}>
+            <div className="w-100">
+            <input type="text" placeholder="Inserisci nuovo Titolo" value={newElement.title} onChange={(e) => setNewElement({ ...newElement, title: e.target.value })} className="w-100"/>
+            {
+                errorTitle === true ? <label className="text-danger">Inserisci almeno 5 caratteri</label> : ''
+            }
+            </div>
+            <div className="w-100">
+            <input type="text" placeholder="Inserisci genere" value={newElement.genre} onChange={(e) => setNewElement({ ...newElement, genre: e.target.value })} className="w-100"/>
+            {
+                errorGenre === true ? <label className="text-danger">Inserisci almeno 5 caratteri</label> : ''
+            }
+            </div>
+            <button type="submit" className="btn btn-outline-secondary align-self-baseline">Aggiungi</button>
         </form>
     )
 }
